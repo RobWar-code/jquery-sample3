c9201d8a9642842a54e085f60c775191a314ee89
RobWar-code <rowaniar@aol.com>
2023-02-05 19:25:24 +0000

    Change to box click color changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-$("p").click(function () {
-    $(this).slideToggle("slow");
+$(document).ready(function () {
+    $(".box").on("click", function () {
+        // Get Class Names
+        classNames = $(this).attr("class").split(" ");
+        boxClass = classNames[0];
+        className = classNames[1];
+        if ($(this).css("background-color") == "rgb(255, 0, 0)") {
+            $("." + className).css("background-color", "#000");
+        }
+        else {
+            $("." + boxClass).css("background-color", "#000");
+            $("." + className).css("background-color", "red");
+        }
+    });
 });
